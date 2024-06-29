b5bf1247da60bc151cbb773aac1663627a31034e
Pedro Henrique Andriotti Bastos <phab@cesar.school>
2024-06-29 18:57:01 -0300

    Starting viewCode Project

---
+++ MinimaList/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinimaList
+//
+//  Created by Pedro Andriotti on 29/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
